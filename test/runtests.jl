5e8374f6478c55a42b55cc9cdf75f35ef2e620b0
hen <hen@posteo.de>
2020-11-03 22:07:43 +0100

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using Test
 
+import Base.==
+
 include("../parser.jl")
+
+Base.:(==)(a::RegexElement, b::RegexElement) = begin
+    a.quantifier == b.quantifier && 
+    a.value == b.value && 
+    a.type == b.type
+end
 
 @testset "test_shift" begin
     a = [1, 2, 3, 4]
@@ -27,11 +35,37 @@ end
     @test (false, 0) == matchesstring(re2, "", 1)
 end
 
+@testset "test_parse" begin
+    str = "a"
+    @test RegexElement(ExactlyOne, Value, 'a') == parse(str)[1]
+    @test 1 == length(parse(str))
+
+    str2 = "abc"
+    @test 3 == length(parse(str2))
+    @test begin
+        RegexElement(ExactlyOne, Value, 'a') == parse(str2)[1] &&
+        RegexElement(ExactlyOne, Value, 'b') == parse(str2)[2] &&
+        RegexElement(ExactlyOne, Value, 'c') == parse(str2)[3]
+    end
+
+    str3 = "a?bc"
+    @test 3 == length(parse(str3))
+    @test begin
+        RegexElement(ZeroOrOne, Value, 'a') == parse(str3)[1] &&
+        RegexElement(ExactlyOne, Value, 'b') == parse(str3)[2] &&
+        RegexElement(ExactlyOne, Value, 'c') == parse(str3)[3]
+    end
+end
+
 @testset "parser" begin
     re1 = parse("a")
-
     @test (true, 1) == test(re1, "a")
-    re = parse("a?bc")
-    @test test(re, "bc") == (true, 2)
-    @test test(re, "abc") == (true, 3)
+
+    re2 = parse("a?")
+    @test (true, 1) == test(re2, "a")
+    @test (true, 0) == test(re2, "")
+
+    re3 = parse("a?bc")
+    @test test(re3, "bc") == (true, 2)
+    @test test(re3, "abc") == (true, 3)
 end
